3d021182334861e6af6460aa36d8386b8e49d1e2
i-linmeng-i <1364876516@qq.com>
2021-03-22 20:36:08 +0800

    去除lua支持

--- ui自定义工具/FunctionClass.cs
+++ ui自定义工具/FunctionClass.cs
@@ -1,5 +1,4 @@
-﻿using NLua;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +11,6 @@ namespace ui自定义工具
         //解析lua
         public void GetLuaInfo()
         {
-            Lua lua = new Lua();
-
-            lua.DoFile(@"D:\源码\repos\ui自定义工具\ui自定义工具\bin\Debug\test.lua");
-
-            lua.RegisterFunction("XCreateWindow",);
-
-            LuaFunction func = lua.GetFunction("test");
-
-            func.Call();
-
-            
         }
         private void creatwindow(string parentid,int l,int t,int w,int h)
         {
